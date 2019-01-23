7e98c2d2c0fd1c447c197a1bd808644b760ad6fd
dunpgemvietnam <dunp@gemvietnam.com>
2019-01-23 10:38:29 +0700

    exclude from solution file for old sample project

--- RedisUsage.ConsoleHandle/Program.cs
+++ RedisUsage.ConsoleHandle/Program.cs
@@ -1,53 +1,53 @@
-﻿using Newtonsoft.Json;
-using RedisUsage.Commands;
-using RedisUsage.Events;
-using RedisUsage.RedisServices;
-using System;
+﻿//using Newtonsoft.Json;
+//using RedisUsage.Commands;
+//using RedisUsage.Events;
+//using RedisUsage.RedisServices;
+//using System;
 
-namespace RedisUsage.ConsoleHandle
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            RedisServices.RedisServices.Init("192.168.15.188", null, "Du@211284");
-            Console.WriteLine(RedisServices.RedisServices.Ping());
+//namespace RedisUsage.ConsoleHandle
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            RedisServices.RedisServices.Init("192.168.15.188", null, "Du@211284");
+//            Console.WriteLine(RedisServices.RedisServices.Ping());
 
-            RedisServices.MessageBussServices.Subscribe<SampleTestCommand>("console handle", (obj) =>
-            {
-                Console.WriteLine("console handle");
-                Console.WriteLine(JsonConvert.SerializeObject(obj));
-                //after process done fire event
-                MessageBussServices.Publish<SampleTestEvent>(new SampleTestEvent()
-                {
-                    Id = Guid.NewGuid()
-                ,
-                    Data = JsonConvert.SerializeObject(new
-                    {
-                        Msg = "Data processed from console handle",
-                        Cmd = obj
-                    })
-                }
-                , MessageBussServices.ProcessType.Topic);
-            });
+//            RedisServices.MessageBussServices.Subscribe<SampleTestCommand>("console handle", (obj) =>
+//            {
+//                Console.WriteLine("console handle");
+//                Console.WriteLine(JsonConvert.SerializeObject(obj));
+//                //after process done fire event
+//                MessageBussServices.Publish<SampleTestEvent>(new SampleTestEvent()
+//                {
+//                    Id = Guid.NewGuid()
+//                ,
+//                    Data = JsonConvert.SerializeObject(new
+//                    {
+//                        Msg = "Data processed from console handle",
+//                        Cmd = obj
+//                    })
+//                }
+//                , MessageBussServices.ProcessType.Topic);
+//            });
 
-            RedisServices.MessageBussServices.Subscribe<SampleTestCommand>("console handle1", (cmd) =>
-            {
-                Console.WriteLine("console handle1");
-                Console.WriteLine(JsonConvert.SerializeObject(cmd));
-            });
-            RedisServices.MessageBussServices.Subscribe<SampleTestCommand>("console handle2", (cmd) =>
-            {
-                Console.WriteLine("console handle2");
-                Console.WriteLine(JsonConvert.SerializeObject(cmd));
-            });
+//            RedisServices.MessageBussServices.Subscribe<SampleTestCommand>("console handle1", (cmd) =>
+//            {
+//                Console.WriteLine("console handle1");
+//                Console.WriteLine(JsonConvert.SerializeObject(cmd));
+//            });
+//            RedisServices.MessageBussServices.Subscribe<SampleTestCommand>("console handle2", (cmd) =>
+//            {
+//                Console.WriteLine("console handle2");
+//                Console.WriteLine(JsonConvert.SerializeObject(cmd));
+//            });
 
-            // can move to other console app for consumer event
-            MessageBussServices.Subscribe<SampleTestEvent>("consumer event handler 1", (evt) =>
-            {
-                Console.WriteLine("Event published from consold handle");
-                Console.WriteLine(JsonConvert.SerializeObject(evt));
-            });
-        }
-    }
-}
+//            // can move to other console app for consumer event
+//            MessageBussServices.Subscribe<SampleTestEvent>("consumer event handler 1", (evt) =>
+//            {
+//                Console.WriteLine("Event published from consold handle");
+//                Console.WriteLine(JsonConvert.SerializeObject(evt));
+//            });
+//        }
+//    }
+//}
--- RedisUsage.Test/Programs.cs
+++ RedisUsage.Test/Programs.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using RedisUsage.Commands;
 using RedisUsage.RedisServices;
 using System;
 using System.Linq;
@@ -16,143 +15,143 @@ namespace RedisUsage.Test
 
         public static void Main(params string[] args)
         {
-            //RedisServices.RedisServices.Init("192.168.15.188", null, "");
-            RedisServices.RedisServices.Init("127.0.0.1", null, "");
-            Console.WriteLine(RedisServices.RedisServices.Ping());
-            HelpGuider();
-            //MessageBussServices.Subscribe<SampleTest>("RedisUsage.Test", (data) => {
-            //    Console.WriteLine("subscribe inline console");
-            //    Console.WriteLine("Recieved to process:");
-            //    Console.WriteLine(JsonConvert.SerializeObject(data));
-            //});
-            while (!appStop)
-            {
-                var cmd = (Console.ReadLine() ?? string.Empty).ToLower();
-                if (cmd.Equals("quit"))
-                {
-                    lock (locker)
-                    {
-                        appStop = true;
-                    }
-                    return;
-                }
-                if (cmd.IndexOf("channel-", StringComparison.OrdinalIgnoreCase) == 0)
-                {
-                    var cmd1 = cmd.Split('-')[1];
-                    switch (cmd1)
-                    {
-                        case "all":
-                            Console.WriteLine(JsonConvert.SerializeObject(MessageBussServices.GetAllChannelInfo()));
-                            break;
-                        case "allname":
-                            Console.WriteLine(JsonConvert.SerializeObject(MessageBussServices.GetAllChannelInfo().Select(i=>i.Name)));
-                            break;
-                        default:
-                            Console.WriteLine("Enter channel name:");
-                            var cname = Console.ReadLine();
-                            switch (cmd1)
-                            {
-                                case "info":
-                                    Console.WriteLine(JsonConvert.SerializeObject(MessageBussServices.GetChannelInfo(cname)));
-                                    break;
-                                case "subscriber":
-                                    Console.WriteLine(JsonConvert.SerializeObject(MessageBussServices.GetSubscribers(cname)));
-                                    break;
-                                case "statistic":
-                                    var info = MessageBussServices.GetChannelInfo(cname);
-                                    Console.WriteLine(JsonConvert.SerializeObject(new
-                                    {
-                                        info.PendingDataQueueLength,
-                                        info.SuccessDataQueueLength,
-                                        info.ErrorDataQueueLength
-                                    }));
-                                    break;
-                                default:
-                                    HelpGuider();
-                                    break;
-                            }
+            ////RedisServices.RedisServices.Init("192.168.15.188", null, "");
+            //RedisServices.RedisServices.Init("127.0.0.1", null, "");
+            //Console.WriteLine(RedisServices.RedisServices.Ping());
+            //HelpGuider();
+            ////MessageBussServices.Subscribe<SampleTest>("RedisUsage.Test", (data) => {
+            ////    Console.WriteLine("subscribe inline console");
+            ////    Console.WriteLine("Recieved to process:");
+            ////    Console.WriteLine(JsonConvert.SerializeObject(data));
+            ////});
+            //while (!appStop)
+            //{
+            //    var cmd = (Console.ReadLine() ?? string.Empty).ToLower();
+            //    if (cmd.Equals("quit"))
+            //    {
+            //        lock (locker)
+            //        {
+            //            appStop = true;
+            //        }
+            //        return;
+            //    }
+            //    if (cmd.IndexOf("channel-", StringComparison.OrdinalIgnoreCase) == 0)
+            //    {
+            //        var cmd1 = cmd.Split('-')[1];
+            //        switch (cmd1)
+            //        {
+            //            case "all":
+            //                Console.WriteLine(JsonConvert.SerializeObject(MessageBussServices.GetAllChannelInfo()));
+            //                break;
+            //            case "allname":
+            //                Console.WriteLine(JsonConvert.SerializeObject(MessageBussServices.GetAllChannelInfo().Select(i=>i.Name)));
+            //                break;
+            //            default:
+            //                Console.WriteLine("Enter channel name:");
+            //                var cname = Console.ReadLine();
+            //                switch (cmd1)
+            //                {
+            //                    case "info":
+            //                        Console.WriteLine(JsonConvert.SerializeObject(MessageBussServices.GetChannelInfo(cname)));
+            //                        break;
+            //                    case "subscriber":
+            //                        Console.WriteLine(JsonConvert.SerializeObject(MessageBussServices.GetSubscribers(cname)));
+            //                        break;
+            //                    case "statistic":
+            //                        var info = MessageBussServices.GetChannelInfo(cname);
+            //                        Console.WriteLine(JsonConvert.SerializeObject(new
+            //                        {
+            //                            info.PendingDataQueueLength,
+            //                            info.SuccessDataQueueLength,
+            //                            info.ErrorDataQueueLength
+            //                        }));
+            //                        break;
+            //                    default:
+            //                        HelpGuider();
+            //                        break;
+            //                }
 
-                            break;
-                    }
-                }
-                else
-                {
-                    CommandUsageTest(cmd);
-                }
-            }
+            //                break;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        CommandUsageTest(cmd);
+            //    }
+            //}
            
-            Console.ReadLine();
+            //Console.ReadLine();
         }
 
         private static void CommandUsageTest(string cmd)
         {
-            switch (cmd)
-            {
-                case "push-queue":
-                    Console.WriteLine("Enter message:");
-                    var msg1 = Console.ReadLine();
-                    for (var i = 0; i < 5; i++)
-                    {
-                        MessageBussServices.Publish<SampleTestCommand>(new SampleTestCommand
-                        {
-                            Message = i + ". " + msg1,
-                            CreatedDate = DateTime.Now
-                        }, MessageBussServices.ProcessType.Queue);
-                    }
-                    break;
-                case "push-stack":
-                    Console.WriteLine("Enter message:");
-                    var msg2 = Console.ReadLine();
-                    for (var i = 0; i < 5; i++)
-                    {
-                        MessageBussServices.Publish<SampleTestCommand>(new SampleTestCommand
-                        {
-                            Message = i + ". " + msg2,
-                            CreatedDate = DateTime.Now
-                        }, MessageBussServices.ProcessType.Stack);
-                    }
+            //switch (cmd)
+            //{
+            //    case "push-queue":
+            //        Console.WriteLine("Enter message:");
+            //        var msg1 = Console.ReadLine();
+            //        for (var i = 0; i < 5; i++)
+            //        {
+            //            MessageBussServices.Publish<SampleTestCommand>(new SampleTestCommand
+            //            {
+            //                Message = i + ". " + msg1,
+            //                CreatedDate = DateTime.Now
+            //            }, MessageBussServices.ProcessType.Queue);
+            //        }
+            //        break;
+            //    case "push-stack":
+            //        Console.WriteLine("Enter message:");
+            //        var msg2 = Console.ReadLine();
+            //        for (var i = 0; i < 5; i++)
+            //        {
+            //            MessageBussServices.Publish<SampleTestCommand>(new SampleTestCommand
+            //            {
+            //                Message = i + ". " + msg2,
+            //                CreatedDate = DateTime.Now
+            //            }, MessageBussServices.ProcessType.Stack);
+            //        }
 
-                    break;
-                case "push":
-                    Console.WriteLine("Enter message:");
-                    var msg = Console.ReadLine();
-                    MessageBussServices.Publish<SampleTestCommand>(new SampleTestCommand
-                    {
-                        Message = msg,
-                        CreatedDate = DateTime.Now
-                    });
-                    break;
-                case "push-mad":
-                    lock (locker)
-                    {
-                        madStop = false;
-                    }
-                    Console.WriteLine("Enter message:");
-                    var msg3 = Console.ReadLine();
-                    new Thread(() =>
-                    {
-                        while (!madStop)
-                        {
-                            MessageBussServices.Publish<SampleTestCommand>(new SampleTestCommand
-                            {
-                                Message = msg3,
-                                CreatedDate = DateTime.Now
-                            });
-                            Thread.Sleep(1000);
-                        }
-                    }).Start();
+            //        break;
+            //    case "push":
+            //        Console.WriteLine("Enter message:");
+            //        var msg = Console.ReadLine();
+            //        MessageBussServices.Publish<SampleTestCommand>(new SampleTestCommand
+            //        {
+            //            Message = msg,
+            //            CreatedDate = DateTime.Now
+            //        });
+            //        break;
+            //    case "push-mad":
+            //        lock (locker)
+            //        {
+            //            madStop = false;
+            //        }
+            //        Console.WriteLine("Enter message:");
+            //        var msg3 = Console.ReadLine();
+            //        new Thread(() =>
+            //        {
+            //            while (!madStop)
+            //            {
+            //                MessageBussServices.Publish<SampleTestCommand>(new SampleTestCommand
+            //                {
+            //                    Message = msg3,
+            //                    CreatedDate = DateTime.Now
+            //                });
+            //                Thread.Sleep(1000);
+            //            }
+            //        }).Start();
 
-                    break;
-                case "stop-mad":
-                    lock (locker)
-                    {
-                        madStop = true;
-                    }
-                    break;
-                default:
-                    HelpGuider();
-                    break;
-            }
+            //        break;
+            //    case "stop-mad":
+            //        lock (locker)
+            //        {
+            //            madStop = true;
+            //        }
+            //        break;
+            //    default:
+            //        HelpGuider();
+            //        break;
+            //}
         }
 
         private static void HelpGuider()
