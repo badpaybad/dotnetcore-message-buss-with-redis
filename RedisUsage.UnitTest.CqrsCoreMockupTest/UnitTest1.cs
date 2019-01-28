633d4f35e707aa0c5cad4f0c31eb0ed77c1ea82d
dunpgemvietnam <dunp@gemvietnam.com>
2019-01-28 10:35:05 +0700

    add project test

---
+++ RedisUsage.UnitTest.CqrsCoreMockupTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedisUsage.UnitTest.CqrsCoreMockupTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ RedisUsage.UnitTest.HandlersAndConsummers/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RedisUsage.UnitTest.HandlersAndConsummers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
